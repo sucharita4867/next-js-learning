585f06f4bfbb9ee964636bc064dbef1f0071744a
sucharita4867 <sucharitasardar7@gmail.com>
2025-12-16 20:16:17 +0530

    create login page and navbar page

---
+++ hello-next/src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const login = () => {
+  return (
+    <div>
+      <h2>this is login page</h2>
+    </div>
+  );
+};
+
+export default login;
---
+++ hello-next/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="border ">
+      <h2 className="text-2xl font-bold">DevStory</h2>
+    </div>
+  );
+};
+
+export default Navbar;
