04ee7e690a186b64fe95e73247d0f35e1a689a56
sucharita4867 <sucharitasardar7@gmail.com>
2025-12-17 13:35:16 +0530

    create navbar in this projects

--- hello-next/src/app/about/page.jsx
+++ hello-next/src/app/about/page.jsx
@@ -1,10 +1,16 @@
 import Title from "@/components/Title";
+import Link from "next/link";
 import React from "react";
 
 const About = () => {
   return (
     <div>
       <Title>welcome to about pages</Title>
+
+      <nav className="space-x-5">
+        <Link href={"/about/contacts"}>Contacts</Link>
+        <Link href={"/about/teams"}>Teams</Link>
+      </nav>
     </div>
   );
 };
--- hello-next/src/components/Navbar.jsx
+++ hello-next/src/components/Navbar.jsx
@@ -1,9 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="border ">
-      <h2 className="text-2xl font-bold">DevStory</h2>
+    <div className="border text-black flex justify-between">
+      <Link href={"/"} className="text-2xl font-bold">
+        DevStory
+      </Link>
+
+      <nav className="space-x-5">
+        <Link href={"/about"}>About</Link>
+        <Link href={"/tutorials"}>Tutorials</Link>
+        <Link href={"/login"}>Login</Link>
+        <Link href={"/register"}>Register</Link>
+      </nav>
     </div>
   );
 };
