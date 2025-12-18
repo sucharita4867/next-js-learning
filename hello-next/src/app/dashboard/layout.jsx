886ad26cbafa39da654be82840a8d0881acdff45
sucharita4867 <sucharitasardar7@gmail.com>
2025-12-18 21:20:54 +0530

    create dashbord pages

---
+++ hello-next/src/app/dashboard/layout.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import React from "react";
+
+const DashboardLayout = ({ children }) => {
+  return (
+    <div className="grid grid-cols-12 gap-5 min-h-screen">
+      <div className="col-span-2 border-r-2">
+        <h3>Navigation</h3>
+        <div className="flex flex-col gap-5 mt-10">
+          <Link
+            className="py-2 px-4 w-full bg-gray-500 rounded"
+            href={"/dashboard/add-story"}
+          >
+            Add Story
+          </Link>
+          <Link
+            className="py-2 px-4 w-full bg-gray-500 rounded"
+            href={"/dashboard/my-profile"}
+          >
+            My Profile
+          </Link>
+          <Link
+            className="py-2 px-4 w-full bg-gray-500 rounded"
+            href={"/dashboard/settings"}
+          >
+            Settings
+          </Link>
+        </div>
+      </div>
+      <div className="col-span-10">{children}</div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
---
+++ hello-next/src/app/dashboard/my-profile/page.jsx
@@ -0,0 +1,12 @@
+import Title from "@/components/Title";
+import React from "react";
+
+const MyProfile = () => {
+  return (
+    <div>
+      <Title>my profile</Title>
+    </div>
+  );
+};
+
+export default MyProfile;
