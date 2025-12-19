70442edd54d5302960a39c4a38dfe7c737945647
sucharita4867 <sucharitasardar7@gmail.com>
2025-12-19 20:29:58 +0530

    added loading page

---
+++ hello-next/src/app/stories/[id]/loading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div>
+      <div className="min-h-screen flex justify-center items-center text-8xl">
+        <h3>L</h3> <span className="animate-spin">🎯</span>
+        <h2>ADING</h2>
+      </div>
+    </div>
+  );
+};
+
+export default loading;
